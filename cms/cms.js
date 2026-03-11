f545373c5962bdbf2723bd9b7b28a951c927b487
Reema Roy <royreema1997@gmail.com>
2026-03-12 00:01:25 +0530

    Update cms.js

--- cms/cms.js
+++ cms/cms.js
@@ -1,30 +1,20 @@
-function fetchProduct(){
-  let url=document.getElementById("link").value;
-  fetch("https://api.allorigins.win/get?url="+encodeURIComponent(url))
-  .then(res=>res.json())
-  .then(data=>{
-  let html=data.contents;
-  let title=html.match(/<title>(.*?)<\/title>/i);
-    
-  if(title){
-  document.getElementById("title").value=title[1];
+async function fetchProduct(){
+
+let url=document.getElementById("link").value;
+
+let api="https://api.microlink.io/?url="+encodeURIComponent(url);
+
+let res=await fetch(api);
+let data=await res.json();
+
+if(data.status=="success"){
+
+document.getElementById("title").value=data.data.title || "";
+
+document.getElementById("description").value=data.data.description || "";
+
+document.getElementById("image").value=data.data.image?.url || "";
+
 }
-});
-}
-
-function preview(){
-  let title=document.getElementById("title").value;
-  let price=document.getElementById("price").value;
-  let image=document.getElementById("image").value;
-  let desc=document.getElementById("description").value;
-
-  document.getElementById("preview").innerHTML=`
-
-<h2>${title}</h2>
-<img src="${image}" width="200">
-<p>${price}</p>
-<p>${desc}</p>
-
-`;
 
 }
