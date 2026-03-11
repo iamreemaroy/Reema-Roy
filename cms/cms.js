ca471b53ea2793bcf93d9a9319fa011db1eec4e5
Reema Roy <royreema1997@gmail.com>
2026-03-12 00:21:13 +0530

    Update cms.js

--- cms/cms.js
+++ cms/cms.js
@@ -7,13 +7,25 @@ let api="https://api.microlink.io/?url="+encodeURIComponent(url);
 let res=await fetch(api);
 let data=await res.json();
 
-if(data.status=="success"){
+if(data.status==="success"){
 
 document.getElementById("title").value=data.data.title || "";
 
 document.getElementById("description").value=data.data.description || "";
 
-document.getElementById("image").value=data.data.image?.url || "";
+document.getElementById("image").value=data.data.image.url || "";
+
+if(url.includes("amazon")){
+
+document.getElementById("platform").value="Amazon";
+
+}
+
+if(url.includes("myntra")){
+
+document.getElementById("platform").value="Myntra";
+
+}
 
 }
 
