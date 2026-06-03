0c03258ff16321713811f8f310eeb8956ec3c0db
Reema Roy <royreema1997@gmail.com>
2026-06-03 17:55:22 +0530

    Create extensions.js

---
+++ cms-v2/src/components/editor/extensions.js
@@ -0,0 +1,42 @@
+import StarterKit from "@tiptap/starter-kit";
+import Placeholder from "@tiptap/extension-placeholder";
+import TaskList from "@tiptap/extension-task-list";
+import TaskItem from "@tiptap/extension-task-item";
+import Image from "@tiptap/extension-image";
+import Link from "@tiptap/extension-link";
+import Underline from "@tiptap/extension-underline";
+
+import Table from "@tiptap/extension-table";
+import TableRow from "@tiptap/extension-table-row";
+import TableCell from "@tiptap/extension-table-cell";
+import TableHeader from "@tiptap/extension-table-header";
+
+export const extensions = [
+  StarterKit,
+
+  Placeholder.configure({
+    placeholder: "Start writing your blog...",
+  }),
+
+  Underline,
+
+  TaskList,
+
+  TaskItem.configure({
+    nested: true,
+  }),
+
+  Image,
+
+  Link.configure({
+    openOnClick: false,
+  }),
+
+  Table.configure({
+    resizable: true,
+  }),
+
+  TableRow,
+  TableHeader,
+  TableCell,
+];
