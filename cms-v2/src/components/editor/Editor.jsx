a36d2e40431e1eba348bbd5f2dba268630750c79
Reema Roy <royreema1997@gmail.com>
2026-06-03 17:53:47 +0530

    Create Editor.jsx

---
+++ cms-v2/src/components/editor/Editor.jsx
@@ -0,0 +1,22 @@
+import { useEditor, EditorContent } from "@tiptap/react";
+import { extensions } from "./extensions";
+import Toolbar from "./Toolbar";
+
+export default function Editor() {
+  const editor = useEditor({
+    extensions,
+    content: "",
+  });
+
+  return (
+    <main className="editor-shell">
+
+      <Toolbar editor={editor} />
+
+      <div className="editor-content">
+        <EditorContent editor={editor} />
+      </div>
+
+    </main>
+  );
+}
