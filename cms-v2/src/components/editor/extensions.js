b013c6e191b9aef02f29b0f96e26d54626acba76
Reema Roy <royreema1997@gmail.com>
2026-06-04 17:06:52 +0530

    Update extensions.js

--- cms-v2/src/components/editor/extensions.js
+++ cms-v2/src/components/editor/extensions.js
@@ -2,6 +2,8 @@ import StarterKit from "@tiptap/starter-kit";
 import Underline from "@tiptap/extension-underline";
 import Link from "@tiptap/extension-link";
 import Image from "@tiptap/extension-image";
+import TaskList from "@tiptap/extension-task-list";
+import TaskItem from "@tiptap/extension-task-item";
 
 import Table from "@tiptap/extension-table";
 import TableRow from "@tiptap/extension-table-row";
