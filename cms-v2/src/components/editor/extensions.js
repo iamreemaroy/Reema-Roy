23b56c0a0a1b298c2a5a478f3d6ab9811b4b8654
Reema Roy <royreema1997@gmail.com>
2026-06-04 15:14:58 +0530

    Update extensions.js

--- cms-v2/src/components/editor/extensions.js
+++ cms-v2/src/components/editor/extensions.js
@@ -6,10 +6,7 @@ import Image from "@tiptap/extension-image";
 import Link from "@tiptap/extension-link";
 import Underline from "@tiptap/extension-underline";
 
-import Table from "@tiptap/extension-table";
-import TableRow from "@tiptap/extension-table-row";
-import TableCell from "@tiptap/extension-table-cell";
-import TableHeader from "@tiptap/extension-table-header";
+
 
 export const extensions = [
   StarterKit,
