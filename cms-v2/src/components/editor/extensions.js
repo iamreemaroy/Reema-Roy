96422bdfa1a4d35efdbc0be1b6abd25d7358becd
Reema Roy <royreema1997@gmail.com>
2026-06-04 17:07:47 +0530

    Update extensions.js

--- cms-v2/src/components/editor/extensions.js
+++ cms-v2/src/components/editor/extensions.js
@@ -2,21 +2,16 @@ import StarterKit from "@tiptap/starter-kit";
 import Underline from "@tiptap/extension-underline";
 import Link from "@tiptap/extension-link";
 import Image from "@tiptap/extension-image";
-import TaskList from "@tiptap/extension-task-list";
-import TaskItem from "@tiptap/extension-task-item";
 
 import Table from "@tiptap/extension-table";
 import TableRow from "@tiptap/extension-table-row";
-import TableCell from "@tiptap/extension-table-cell";
 import TableHeader from "@tiptap/extension-table-header";
+import TableCell from "@tiptap/extension-table-cell";
+
+import TaskList from "@tiptap/extension-task-list";
+import TaskItem from "@tiptap/extension-task-item";
 
 export const extensions = [
-  TaskList,
-
-TaskItem.configure({
-  nested: true,
-}),
-  
   StarterKit,
 
   Underline,
@@ -36,4 +31,10 @@ TaskItem.configure({
   TableRow,
   TableHeader,
   TableCell,
+
+  TaskList,
+
+  TaskItem.configure({
+    nested: true,
+  }),
 ];
