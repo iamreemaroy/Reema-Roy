d5b1e72ccd21b857524e17bfaf64a24ed0095361
Reema Roy <royreema1997@gmail.com>
2026-06-04 15:33:09 +0530

    Update extensions.js

--- cms-v2/src/components/editor/extensions.js
+++ cms-v2/src/components/editor/extensions.js
@@ -1,33 +1,18 @@
 import StarterKit from "@tiptap/starter-kit";
-import Placeholder from "@tiptap/extension-placeholder";
-import TaskList from "@tiptap/extension-task-list";
-import TaskItem from "@tiptap/extension-task-item";
-import Image from "@tiptap/extension-image";
-import Link from "@tiptap/extension-link";
 import Underline from "@tiptap/extension-underline";
-
-
+import Link from "@tiptap/extension-link";
+import Image from "@tiptap/extension-image";
 
 export const extensions = [
   StarterKit,
 
-  Placeholder.configure({
-    placeholder: "Start writing your blog...",
-  }),
-
   Underline,
-
-  TaskList,
-
-  TaskItem.configure({
-    nested: true,
-  }),
-
-  Image,
 
   Link.configure({
     openOnClick: false,
   }),
 
-  
-
+  Image.configure({
+    inline: false,
+  }),
+];
